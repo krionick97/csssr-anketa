db4d6531e286e0bfcbe40505ae0022a6a83ac94b
Vlad_Kuznetcov <mistermers8@gmail.com>
2019-07-01 22:01:26 +0300

    Click checkboxes

--- src/modules/header.component.js
+++ src/modules/header.component.js
@@ -10,4 +10,30 @@ $(document).ready(function () {
   //   fontFamily: 'sans-serif'
   // })
   // .appendTo($('header'))
+  //--------------------------------------------------
+
+  // Click checkobox
+  let checkboxIndex = new Array();
+  let markIndex = new Array();
+  let optionIndex = new Array();
+
+  for(let j=1; j<20; j++) {
+    checkboxIndex[j] = 'box' + j;
+    markIndex[j] = 'box-mark-' + j;
+    optionIndex[j] = 'option' + j;
+
+    document.getElementById(checkboxIndex[j]).onclick = function() {
+      if(document.getElementById(optionIndex[j]).checked !== true) {
+        document.getElementById(markIndex[j]).style.opacity = 1;
+        document.getElementById(optionIndex[j]).checked = true;
+      } else {
+        document.getElementById(markIndex[j]).style.opacity = 0;
+        document.getElementById(optionIndex[j]).checked = false;
+      }
+
+    } // end click function
+
+        
+  } // end for
+  
 });
