d1220449c53d6216a87ec9dbe2a27dcefeaea374
Vlad_Kuznetcov <mistermers8@gmail.com>
2019-07-02 23:23:56 +0300

    JS level inputs

--- src/modules/header.component.js
+++ src/modules/header.component.js
@@ -12,18 +12,23 @@ $(document).ready(function () {
   // .appendTo($('header'))
   //--------------------------------------------------
 
-  // Click checkobox about yourself
+  // Variables
   let checkboxIndex = new Array();
   let markIndex = new Array();
   let optionIndex = new Array();
+  let JSlevelIndex = new Array();
+  let arrlIndex = new Array();
+  let itemLevelIndex = new Array();
+  // -------------------------------------------------------
 
+  // Click checkobox about yourself
   for(let j=1; j<20; j++) {
     checkboxIndex[j] = 'box' + j;
     markIndex[j] = 'box-mark-' + j;
     optionIndex[j] = 'option' + j;
 
     document.getElementById(checkboxIndex[j]).onclick = function() {
-      if(document.getElementById(optionIndex[j]).checked !== true) {
+      if(document.getElementById(optionIndex[j]).checked != true) {
         document.getElementById(markIndex[j]).style.opacity = 1;
         document.getElementById(optionIndex[j]).checked = true;
       } else {
@@ -33,5 +38,28 @@ $(document).ready(function () {
     } // end click function
   } // end for
   // -------------------------------------------------------------------------------------
-  
+
+  // JS Level options
+  for(let i=1; i<5; i++) {
+    JSlevelIndex[i] = 'level' + i;
+    arrlIndex[i] = 'arr-option-' + i;
+    itemLevelIndex[i] = 'item-level-' + i;
+
+    // if(document.getElementById(JSlevelIndex[i]).checked == false) {
+    //   document.getElementById(arrlIndex[i]).style.display = 'none';
+    // } else {
+    //   document.getElementById(arrlIndex[i]).style.display = 'block';
+    // }
+    if(document.getElementById(JSlevelIndex[i]).checked == false) {
+      document.getElementById(arrlIndex[i]).style.visibility = 'hidden';
+    } else {
+      document.getElementById(arrlIndex[i]).style.visibility = 'visible';
+    }
+
+    // document.getElementById(itemLevelIndex[i]).onclick = function() {
+    //   if(document.getElementById(JSlevelIndex[i]).checked == false) {
+    //     document.getElementById(JSlevelIndex[i]).checked = true;
+    //   } // end if
+    // } // end click funtion
+  } // end for
 });
